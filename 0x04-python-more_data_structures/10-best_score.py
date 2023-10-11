abb6dd55276bd5c8009d04569feb051b17cbcb5e
Georgen21 <chukwudinwabia42@gmail.com>
2023-10-11 05:58:26 -0700

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        best_s = max(a_dictionary, key=a_dictionary.get)
+        return best_s
+    else:
+        return None
